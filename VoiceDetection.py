975c427452530d99f97a0f80d8e2a60574380389
JonasFortes12 <jonascforte@hotmail.com>
2023-09-18 16:56:09 -0300

    feat: Divide signals in 80 blocs and calculate the energies for each bloc
    Question 02

--- VoiceDetection.py
+++ VoiceDetection.py
@@ -16,7 +16,7 @@ audio03Data = audiosMatrix[:, 2]
 audio04Data = audiosMatrix[:, 3]
 audio05Data = audiosMatrix[:, 4]
 
-# Separae os sinais de áudio 'SIM'
+# Separar os sinais de áudio 'SIM'
 audio06Data = audiosMatrix[:, 5]
 audio07Data = audiosMatrix[:, 6]
 audio08Data = audiosMatrix[:, 7]
@@ -70,9 +70,87 @@ plt.legend()
 # Exibir os gráficos
 plt.show()
 
+# ________________________________Questão 02________________________________________
 
 
+# Dividir os sinais de áudio 'SIM' e 'NÃO' em 80 blocos de N/80 amostras
+divisionNumber = 80
+audio01Divided = np.array_split(audio01Data, divisionNumber)
+audio02Divided = np.array_split(audio02Data, divisionNumber)
+audio03Divided = np.array_split(audio03Data, divisionNumber)
+audio04Divided = np.array_split(audio04Data, divisionNumber)
+audio05Divided = np.array_split(audio05Data, divisionNumber)
+audio06Divided = np.array_split(audio06Data, divisionNumber)
+audio07Divided = np.array_split(audio07Data, divisionNumber)
+audio08Divided = np.array_split(audio08Data, divisionNumber)
+audio09Divided = np.array_split(audio09Data, divisionNumber)
+audio10Divided = np.array_split(audio10Data, divisionNumber)
+
+# Instânciar vetores para armazenar as energias dos blocos de sinais
+audio01Energies = []
+audio02Energies = []
+audio03Energies = []
+audio04Energies = []
+audio05Energies = []
+audio06Energies = []
+audio07Energies = []
+audio08Energies = []
+audio09Energies = []
+audio10Energies = []
+
+# Calcular a energia de cada bloco nos 10 sinais de áudio
+for i in range(divisionNumber):
+    audio01Energies.append(np.sum(np.square(audio01Divided[i])))
+    audio02Energies.append(np.sum(np.square(audio02Divided[i])))
+    audio03Energies.append(np.sum(np.square(audio03Divided[i])))
+    audio04Energies.append(np.sum(np.square(audio04Divided[i])))
+    audio05Energies.append(np.sum(np.square(audio05Divided[i])))
+    audio06Energies.append(np.sum(np.square(audio06Divided[i])))
+    audio07Energies.append(np.sum(np.square(audio07Divided[i])))
+    audio08Energies.append(np.sum(np.square(audio08Divided[i])))
+    audio09Energies.append(np.sum(np.square(audio09Divided[i])))
+    audio10Energies.append(np.sum(np.square(audio10Divided[i])))
+
+# Definir valores do eixo X
+x = np.arange(0, divisionNumber)
 
 
+# Criar uma figura para o gráfico de energias do áudios 'NÃO'
+plt.figure()
+
+# Plotar os sinais de áudio 'NÃO' 
+plt.plot(x, audio01Energies, label='audio01', color='red')
+plt.plot(x, audio02Energies, label='audio02', color='blue')
+plt.plot(x, audio03Energies, label='audio03', color='green')
+plt.plot(x, audio04Energies, label='audio04', color='black')
+plt.plot(x, audio05Energies, label='audio05', color='orange')
+
+# Adicionar rótulos aos eixos
+plt.xlabel('Bloco')
+plt.ylabel('Energia')
+# Adicionar um título ao gráfico
+plt.title('Energia dos sinais de áudio "NÃO"')
+# Adicionar uma legenda
+plt.legend()
 
 
+# Criar uma figura para o gráfico de energias do áudios 'SIM'
+plt.figure()
+
+# Plotar os sinais de áudio 'SIM' 
+plt.plot(x, audio06Energies, label='audio06', color='red')
+plt.plot(x, audio07Energies, label='audio07', color='blue')
+plt.plot(x, audio08Energies, label='audio08', color='green')
+plt.plot(x, audio09Energies, label='audio09', color='black')
+plt.plot(x, audio10Energies, label='audio10', color='orange')
+
+# Adicionar rótulos aos eixos
+plt.xlabel('Bloco')
+plt.ylabel('Energia')
+# Adicionar um título ao gráfico
+plt.title('Energia dos sinais de áudio "SIM"')
+# Adicionar uma legenda
+plt.legend()
+
+# Exibir os gráficos
+plt.show()
